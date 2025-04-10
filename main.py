b295e179c55fb91334c3a0bf9da83577a46c03aa
Zenx01 <hddldgd42@gmail.com>
2025-04-10 20:42:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import instaloader
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Instagram downloader is running!'
+
+@app.route('/api', methods=['GET'])
+def download_instagram_video():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({'status': 'error', 'message': 'URL is missing'})
+
+    # Load instaloader with sessionid
+    L = instaloader.Instaloader()
+    sessionid = '73394703601%3AJ9E4cS9whE7pQ5%3A2%3AAYf8u6aehuRKRY22DAXDBjSV9GjAtQq_ZzqZKyAGtQ'  # Replace with your sessionid
+    L.context._session.cookies.set('sessionid', sessionid, domain='.instagram.com')
+
+    try:
+        shortcode = url.split('/reel/')[1].split('/')[0]
+        post = instaloader.Post.from_shortcode(L.context, shortcode)
+        video_url = post.video_url
+
+        return jsonify({'status': 'success', 'video_url': video_url})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
